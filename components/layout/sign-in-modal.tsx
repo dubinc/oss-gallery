d5b56376ebff01fa5201e909c772159c2a0c015e
ansub <khanansub3@gmail.com>
2024-04-26 18:16:47 +0530

    fix: changed logo path from .png to .jpg

--- components/layout/sign-in-modal.tsx
+++ components/layout/sign-in-modal.tsx
@@ -23,7 +23,7 @@ const SignInModal = ({
       <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center md:px-16">
         <a href="https://oss.gallery">
           <Image
-            src="/logo.png"
+            src="/logo.jpg"
             alt="Logo"
             className="h-10 w-10 rounded-full"
             width={20}
