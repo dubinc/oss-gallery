d58e9c772fb513f71c826b2d12bff93cfada2883
Steven Tey <stevensteel97@gmail.com>
2024-04-29 18:58:30 +0300

    add script

---
+++ scripts/download.ts
@@ -0,0 +1,25 @@
+import prisma from "@/lib/prisma";
+import "dotenv-flow/config";
+import * as fs from "fs";
+
+async function main() {
+  const projects = await prisma.project.findMany({
+    select: {
+      id: true,
+      name: true,
+      description: true,
+      slug: true,
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
+
+  // save as JSONL
+  const file = fs.createWriteStream("projects.jsonl");
+  projects.forEach((project) => {
+    file.write(JSON.stringify(project) + "\n");
+  });
+}
+
+main();
