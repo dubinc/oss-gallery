acc72af1c4ed3cc8fa12afa7bdde450e3020ebaf
Steven Tey <stevensteel97@gmail.com>
2024-04-30 14:43:18 +0300

    Update project-layout-tabs.tsx

--- components/projects/project-layout-tabs.tsx
+++ components/projects/project-layout-tabs.tsx
@@ -16,7 +16,11 @@ export default function ProjectLayoutTabs() {
   return (
     <div className="my-4 flex flex-col space-y-6 p-4">
       <div className="flex max-w-fit items-center rounded-full bg-gray-100">
-        <TabLink title="Analytics" href={`/projects/${slug}`} active={!tab} />
+        <TabLink
+          title="Analytics"
+          href={`/projects/${slug}`}
+          active={tab[0] === slug}
+        />
         {PROJECT_TABS.map((t) => (
           <TabLink
             key={t.tab}
